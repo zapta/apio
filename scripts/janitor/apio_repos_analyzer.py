d5d22161ae29be4a95b2ee7d4919552d00477745
Zapta <zapta@users.noreply.github.com>
2026-09-08 21:03:13 -0700

    wip

--- scripts/janitor/apio_repos_analyzer.py
+++ scripts/janitor/apio_repos_analyzer.py
@@ -8,7 +8,14 @@ import json
 import argparse
 from pathlib import Path
 from typing import List, Dict
-from scripts.janitor.apio_repos_crawler import CrawlResults, GithubReleaseRef, PypiCrawl
+from scripts.janitor.apio_repos_crawler import (
+    CrawlResults,
+    GithubReleaseRef,
+    PypiCrawl,
+    PypiReleaseCrawl,
+    VscodeMarketplaceCrawl,
+    VscodeReleaseCrawl,
+)
 
 parser = argparse.ArgumentParser(description="Crawl apio repos")
 parser.add_argument(
